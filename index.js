0fffdb8f226eac2311e9875d595a303313b4e0d6
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-04 06:34:18 +0600

    catagories jon entered

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.get('/',(req, res)=>{
-    res.send('chekking backend index js')
-
-    app.listen(port, () =>{
-        console.log(`Food loader us running: ${port}`)
-    })
+app.get('/',(req, res) =>{
+    res.send('chekking backend index js');
 
 })
+app.listen(port, () =>{
+    console.log(`Food loader Is Cooking on: ${port}`)
+});
