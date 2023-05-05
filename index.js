94bbbf64bad0a32466ddcb5b178d1215f01756dc
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-05 23:22:54 +0600

    last commit

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const express = require('express')
 const app = express();
-const cors = require('corse');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const catagories = require('./data/catagories.json');
+const allData = require('./data/allData.json');
 const recipes = require('./data/recipes.json');
 
 app.use(cors());
 
-app.get('/',(req, res) =>{
+app.get('/', (req, res) => {
     res.send('chekking backend index js');
-    
 })
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Food loader Is Cooking on: ${port}`)
 });
 
-// catagories data section 
-app.get('/catagories', (req, res) =>{
-    res.send(catagories);
-})
+app.get('/alldata', (req, res) => {
+    res.send(allData);
+});
+
+app.get("/allData/:id", (req, res) => {
+    const id = req.params.id;
+    let item = null;
+    allChefs.forEach((obj) => {
+      const chef = obj.chefs.find((c) => c.id == id);
+      if (chef) {
+        item = chef;
+      }
+    });
+    if (item) {
+      res.send({ item });
+    } else {
+      res.status(404).send({ error: `Chef with ID ${id} not found` });
+    }
+  });
+// app.get("/allData/:id", (req, res) => {
+//     const id = req.params.id;
+//     // console.log(id);
+//     const selectedData =
+//     allData.find(d => d.id === id);
+//     res.send(selectedData);
+// });
+
 
 // recipes data section 
-app.get('/recipes',(req , res) =>{
+app.get('/recipes', (req, res) => {
     res.send(recipes);
 })
